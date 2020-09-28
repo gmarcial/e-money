7567a4c9f4b132788114c0844eeb64eabb4663f6
Guilherme Marcial <guilherme.marcial.felipe@gmail.com>
2020-09-28 13:42:17 -0400

    build(terraform): changes in configuration of firewall to meet the needs of project

--- deployments/terraform/modules/firewall/main.tf
+++ deployments/terraform/modules/firewall/main.tf
@@ -6,76 +6,108 @@ terraform {
     }
   }
 }
-
-resource "digitalocean_firewall" "firewall" {
-  name = "emoney-firewall"
-
-  inbound_rule {
-    protocol           = "tcp"
-    port_range         = "443"
-    source_droplet_ids = [var.application.id]
-    source_addresses   = [var.gateway.ip]
-  }
-
-  inbound_rule {
-    protocol           = "tcp"
-    port_range         = "22"
-    source_droplet_ids = [var.application.id]
-    source_addresses   = [var.gateway.ip]
-  }
-
-  inbound_rule {
-    protocol           = "tcp"
-    port_range         = "5432"
-    source_droplet_ids = [var.application_database.id]
-    source_addresses   = [var.application.ip]
-  }
-
-  inbound_rule {
-    protocol           = "tcp"
-    port_range         = "5432"
-    source_droplet_ids = [var.vault_database.id]
-    source_addresses   = [var.vault.ip]
-  }
-
-  inbound_rule {
-    protocol           = "tcp"
-    port_range         = "8200"
-    source_droplet_ids = [var.vault.id]
-    source_addresses   = [var.application.ip]
-  }
-
-  inbound_rule {
-    protocol           = "tcp"
-    port_range         = "443"
-    source_droplet_ids = [var.gateway.id]
-    source_addresses   = ["0.0.0.0/0", "::/0"]
-  }
+resource "digitalocean_firewall" "default_inbound" {
+  name = "default-inbound"
 
   inbound_rule {
     protocol           = "tcp"
     port_range         = "22"
     source_droplet_ids = [var.gateway.id]
-    source_addresses   = ["0.0.0.0/0", "::/0"]
+    source_addresses = [var.ip_admin]
   }
+
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "1-65535"
+    source_droplet_ids = [var.gateway.id]
+  }
+
+  inbound_rule {
+    protocol           = "udp"
+    port_range         = "1-65535"
+    source_droplet_ids = [var.gateway.id]
+  }
+}
+
+resource "digitalocean_firewall" "default_outbound" {
+  name = "default-outbound"
 
   outbound_rule {
     protocol              = "icmp"
-    destination_droplet_ids    = [var.gateway.id, var.application.id, var.vault.id, var.application_database.id, var.vault_database.id]
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
     protocol              = "tcp"
     port_range = "1-65535"
-    destination_droplet_ids    = [var.gateway.id, var.application.id, var.vault.id, var.application_database.id, var.vault_database.id]
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
     protocol              = "udp"
     port_range = "1-65535"
-    destination_droplet_ids    = [var.gateway.id, var.application.id, var.vault.id, var.application_database.id, var.vault_database.id]
     destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_firewall" "gateway_firewall" {
+  name = "gateway-rules"
+
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "22"
+    source_addresses = [var.ip_admin]
+  }
+
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "1-65535"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol           = "udp"
+    port_range         = "1-65535"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_firewall" "application_firewall" {
+  name = "application-rules"
+
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "443"
+    source_droplet_ids = [var.gateway.id]
+  }
+}
+
+resource "digitalocean_firewall" "vault_firewall" {
+  name = "vault-rules"
+
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "8200"
+    source_droplet_ids = [var.application.id]
+  }
+}
+
+resource "digitalocean_firewall" "application_database_firewall" {
+  name = "application-database-rules"
+
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "5432"
+    source_droplet_ids = [var.application.id]
+  }
+}
+
+resource "digitalocean_firewall" "vault_database_firewall" {
+  name = "vault-database-rules"
+
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "5432"
+    source_droplet_ids = [var.vault.id]
   }
 }
